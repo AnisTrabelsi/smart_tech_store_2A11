f4a51e3f8471d129516d097621936894220dc343
Too lazy To name It <2lazy2nameit@gmail.com>
2022-04-16 12:56:26 +0100

    before int eger

--- build-Atelier_Connexion-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-Atelier_Connexion-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Commande/mainwindow.h"
+#include "../../Categorie/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[10];
-    char stringdata0[171];
+    QByteArrayData data[9];
+    char stringdata0[149];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,17 +37,16 @@ QT_MOC_LITERAL(2, 30, 0), // ""
 QT_MOC_LITERAL(3, 31, 23), // "on_pb_supprimer_clicked"
 QT_MOC_LITERAL(4, 55, 24), // "on_cherchermodif_clicked"
 QT_MOC_LITERAL(5, 80, 20), // "on_valider_3_clicked"
-QT_MOC_LITERAL(6, 101, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(7, 123, 14), // "on_NUM_clicked"
-QT_MOC_LITERAL(8, 138, 18), // "on_PAIMENT_clicked"
-QT_MOC_LITERAL(9, 157, 13) // "on_ID_clicked"
+QT_MOC_LITERAL(6, 101, 14), // "on_NUM_clicked"
+QT_MOC_LITERAL(7, 116, 18), // "on_PAIMENT_clicked"
+QT_MOC_LITERAL(8, 135, 13) // "on_ID_clicked"
 
     },
     "MainWindow\0on_valider_clicked\0\0"
     "on_pb_supprimer_clicked\0"
     "on_cherchermodif_clicked\0on_valider_3_clicked\0"
-    "on_pushButton_clicked\0on_NUM_clicked\0"
-    "on_PAIMENT_clicked\0on_ID_clicked"
+    "on_NUM_clicked\0on_PAIMENT_clicked\0"
+    "on_ID_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -57,7 +56,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -65,17 +64,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08 /* Private */,
-       3,    0,   55,    2, 0x08 /* Private */,
-       4,    0,   56,    2, 0x08 /* Private */,
-       5,    0,   57,    2, 0x08 /* Private */,
-       6,    0,   58,    2, 0x08 /* Private */,
-       7,    0,   59,    2, 0x08 /* Private */,
-       8,    0,   60,    2, 0x08 /* Private */,
-       9,    0,   61,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    0,   54,    2, 0x08 /* Private */,
+       8,    0,   55,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -97,10 +94,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->on_pb_supprimer_clicked(); break;
         case 2: _t->on_cherchermodif_clicked(); break;
         case 3: _t->on_valider_3_clicked(); break;
-        case 4: _t->on_pushButton_clicked(); break;
-        case 5: _t->on_NUM_clicked(); break;
-        case 6: _t->on_PAIMENT_clicked(); break;
-        case 7: _t->on_ID_clicked(); break;
+        case 4: _t->on_NUM_clicked(); break;
+        case 5: _t->on_PAIMENT_clicked(); break;
+        case 6: _t->on_ID_clicked(); break;
         default: ;
         }
     }
@@ -132,13 +128,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 7;
     }
     return _id;
 }
